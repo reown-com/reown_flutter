6bf364160555e4a29905bffe28d1245beb6e8880
Alfreedom <00tango.bromine@icloud.com>
2025-05-15 14:59:45 +0200

    [feature] Added Sentry (#161)
    * Added Sentry
    
    * DSN on environment

--- packages/reown_appkit/example/base/windows/flutter/generated_plugin_registrant.cc
+++ packages/reown_appkit/example/base/windows/flutter/generated_plugin_registrant.cc
@@ -7,11 +7,14 @@
 #include "generated_plugin_registrant.h"
 
 #include <connectivity_plus/connectivity_plus_windows_plugin.h>
+#include <sentry_flutter/sentry_flutter_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   ConnectivityPlusWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("ConnectivityPlusWindowsPlugin"));
+  SentryFlutterPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("SentryFlutterPlugin"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
