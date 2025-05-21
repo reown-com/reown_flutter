1fbab2598ed9a89fda660b0fd55b5fa8899801d0
Alfreedom <00tango.bromine@icloud.com>
2025-05-21 10:53:04 +0200

    trying polkadart

--- packages/reown_appkit/example/base/lib/utils/crypto/polkadot.dart
+++ packages/reown_appkit/example/base/lib/utils/crypto/polkadot.dart
@@ -1,3 +1,9 @@
+import 'package:polkadart/polkadart.dart';
+import 'package:convert/convert.dart';
+import 'dart:typed_data';
+
+import 'package:reown_appkit/reown_appkit.dart';
+
 enum PolkadotMethods {
   polkadotSignTransaction,
   polkadotSignMessage,
@@ -41,4 +47,42 @@ class Polkadot {
         'tip': '0x00000000000000000000000000000000',
         'version': 4,
       };
+
+  // https://polkadot.subscan.io/extrinsic/26099230-2
+  static Map<String, dynamic> createExtrinsic(String address) {
+    // Convert the address to a Uint8List for the signer
+    final signer = PolkadotChainUtils.ss58AddressToPublicKey(address);
+
+    // Balances.transfer_keep_alive method
+    final method =
+        Uint8List.fromList(hex.decode('0x0403'.replaceAll('0x', '')));
+
+    // Real signature from the transaction
+    final signature = Uint8List.fromList(hex.decode(
+      '0xfd65726905fb5e484613834fbba1101201b25117aca99cc0805ee2ff640285238ea9047ed7010b165610bba2b5abf2a702ccf19e6562e36d8b1c5176eee95f04'
+          .replaceAll('0x', ''),
+    ));
+
+    return ExtrinsicPayload(
+      signer: Uint8List.fromList(signer),
+      method: method,
+      signature: signature,
+      eraPeriod: 32, // Should be determined from the chain's current era
+      blockNumber: 26099230, // Actual block number from the transaction
+      nonce: 34579, // Actual nonce from the transaction
+      tip: 0, // No tip in this transaction
+      // customSignedExtensions: {
+      //   'CheckNonZeroSender': null,
+      //   'CheckSpecVersion': '0x00002468', // From the transaction payload
+      //   'CheckTxVersion': '0x0000000e', // From the transaction payload
+      //   'CheckGenesis':
+      //       '0xe143f23803ac50e8f6f8e62695d1ce9e4e1d68aa36c1cd2cfd15340213f3423e', // From the transaction payload
+      //   'CheckMortality':
+      //       '0x554d682a74099d05e8b7852d19c93b527b5fae1e9e1969f6e1b82a2f09a14cc9', // From the transaction payload
+      //   'CheckNonce': 34579, // Same as the nonce
+      //   'CheckWeight': null,
+      //   'ChargeTransactionPayment': 0, // No tip
+      // },
+    ).toEncodedMap(null);
+  }
 }
