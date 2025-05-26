8e3c848ac75d5575edbf0a03d6bfe0af11962327
Alfreedom <00tango.bromine@icloud.com>
2025-05-26 11:12:55 +0200

    XRPL tv implmentation

--- packages/reown_core/lib/models/tvf_data.dart
+++ packages/reown_core/lib/models/tvf_data.dart
@@ -44,5 +44,8 @@ class TVFData {
     'solana_signTransaction',
     'solana_signAllTransactions',
     'wallet_sendCalls',
+    // XRPL
+    'xrpl_signTransaction',
+    'xrpl_signTransactionFor',
   ];
 }
