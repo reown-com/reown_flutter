1f50ed3074694a9f0e93adb2c40fa11badbcf41b
Alfreedom <00tango.bromine@icloud.com>
2025-05-26 18:15:07 +0200

    revert accidental change

--- packages/reown_core/lib/models/tvf_data.dart
+++ packages/reown_core/lib/models/tvf_data.dart
@@ -40,7 +40,7 @@ class TVFData {
   static final tvfRequestMethods = [
     'eth_sendTransaction',
     'eth_sendRawTransaction',
-    'solana_signTransaction',
+    'solana_signAndSendTransaction',
     'solana_signTransaction',
     'solana_signAllTransactions',
     'wallet_sendCalls',
