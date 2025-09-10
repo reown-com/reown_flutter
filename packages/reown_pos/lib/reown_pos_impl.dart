8556e1a2d09e11292f5e08c1459d482c7ca74fea
Alfreedom <00tango.bromine@icloud.com>
2025-09-10 18:58:19 +0200

    rollback

--- packages/reown_pos/lib/reown_pos_impl.dart
+++ packages/reown_pos/lib/reown_pos_impl.dart
@@ -197,7 +197,10 @@ class ReownPos with BlockchainService, ValidatorService implements IReownPos {
       }
     } on ReownCoreError catch (e) {
       _reOwnCore!.logger.e('[$runtimeType] connect error: $e');
-      onPosEvent.broadcast(ConnectFailedEvent(e.message));
+      // If session request fails due to the POS restar function we don't need to propagate this error.
+      if (e.code != 5090 && e.message != 'ABORTED') {
+        onPosEvent.broadcast(ConnectFailedEvent(e.message));
+      }
     } catch (e, s) {
       _reOwnCore!.logger.e('[$runtimeType] connect error: $e, $s');
       onPosEvent.broadcast(ConnectFailedEvent(e.toString()));
