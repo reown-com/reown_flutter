18891335172a8334f1bf62a9faf25679ea781df5
Alfreedom <00tango.bromine@icloud.com>
2025-09-29 13:48:31 +0200

    First steps

--- packages/reown_walletkit/example/lib/models/chain_data.dart
+++ packages/reown_walletkit/example/lib/models/chain_data.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:reown_walletkit_wallet/models/chain_metadata.dart';
-import 'package:reown_walletkit_wallet/utils/dart_defines.dart';
 
 class ChainsDataList {
   static final List<ChainMetadata> eip155Chains = [
@@ -11,7 +10,9 @@ class ChainsDataList {
       logo:
           'https://pbs.twimg.com/profile_images/1878738447067652096/tXQbWfpf_400x400.jpg',
       color: Colors.blue.shade300,
-      rpc: ['https://eth.drpc.org'],
+      rpc: [
+        'https://rpc.walletconnect.org/v1',
+      ],
     ),
     ChainMetadata(
       type: ChainType.eip155,
@@ -20,7 +21,9 @@ class ChainsDataList {
       logo:
           'https://pbs.twimg.com/profile_images/1881552782148206592/uL2NdckM_400x400.jpg',
       color: Colors.purple.shade300,
-      rpc: ['https://polygon-rpc.com/'],
+      rpc: [
+        'https://rpc.walletconnect.org/v1',
+      ],
     ),
     ...chainAbstraction,
     const ChainMetadata(
@@ -30,7 +33,9 @@ class ChainsDataList {
       logo:
           'https://pbs.twimg.com/profile_images/1923466301227532288/TuL8kPq3_400x400.jpg',
       color: Colors.orange,
-      rpc: ['https://api.avax.network/ext/bc/C/rpc'],
+      rpc: [
+        'https://rpc.walletconnect.org/v1',
+      ],
     ),
     const ChainMetadata(
       type: ChainType.eip155,
@@ -39,7 +44,9 @@ class ChainsDataList {
       logo:
           'https://pbs.twimg.com/profile_images/1876286110071975936/HvkhyFZg_400x400.jpg',
       color: Colors.orange,
-      rpc: ['https://bsc-dataseed1.bnbchain.org'],
+      rpc: [
+        'https://rpc.walletconnect.org/v1',
+      ],
     ),
     const ChainMetadata(
       type: ChainType.eip155,
@@ -48,7 +55,9 @@ class ChainsDataList {
       logo:
           'https://pbs.twimg.com/profile_images/1613170131491848195/InjXBNx9_400x400.jpg',
       color: Colors.green,
-      rpc: ['https://forno.celo.org/'],
+      rpc: [
+        'https://rpc.walletconnect.org/v1',
+      ],
     ),
     const ChainMetadata(
       type: ChainType.eip155,
@@ -57,7 +66,9 @@ class ChainsDataList {
       logo:
           'https://pbs.twimg.com/profile_images/1846528977428480001/h_MlabDj_400x400.jpg',
       color: Colors.greenAccent,
-      rpc: ['https://rpc.gnosischain.com/'],
+      rpc: [
+        'https://rpc.walletconnect.org/v1',
+      ],
     ),
     const ChainMetadata(
       type: ChainType.eip155,
@@ -66,7 +77,9 @@ class ChainsDataList {
       logo:
           'https://pbs.twimg.com/profile_images/1835668010951950336/Aq1Kg1p0_400x400.jpg',
       color: Colors.black,
-      rpc: ['https://mainnet.era.zksync.io'],
+      rpc: [
+        'https://rpc.walletconnect.org/v1',
+      ],
     ),
     ChainMetadata(
       type: ChainType.eip155,
@@ -75,7 +88,9 @@ class ChainsDataList {
       logo: 'https://avatars.githubusercontent.com/u/179229932?s=200&v=4',
       color: Colors.blue.shade300,
       isTestnet: true,
-      rpc: ['https://ethereum-sepolia.publicnode.com'],
+      rpc: [
+        'https://rpc.walletconnect.org/v1',
+      ],
     ),
     ChainMetadata(
       type: ChainType.eip155,
@@ -105,7 +120,9 @@ class ChainsDataList {
       logo:
           'https://pbs.twimg.com/profile_images/1882673922400776192/niCbzDud_400x400.jpg',
       color: Colors.blue,
-      rpc: ['https://arbitrum.blockpi.network/v1/rpc/public'],
+      rpc: [
+        'https://rpc.walletconnect.org/v1',
+      ],
     ),
     const ChainMetadata(
       type: ChainType.eip155,
@@ -114,7 +131,9 @@ class ChainsDataList {
       logo:
           'https://pbs.twimg.com/profile_images/1734354549496836096/-laoU9C9_400x400.jpg',
       color: Colors.red,
-      rpc: ['https://mainnet.optimism.io/'],
+      rpc: [
+        'https://rpc.walletconnect.org/v1',
+      ],
     ),
     const ChainMetadata(
       type: ChainType.eip155,
@@ -123,7 +142,9 @@ class ChainsDataList {
       logo:
           'https://pbs.twimg.com/profile_images/1840800810571350019/1pCjLY5q_400x400.jpg',
       color: Colors.lightBlue,
-      rpc: ['https://mainnet.base.org'],
+      rpc: [
+        'https://rpc.walletconnect.org/v1',
+      ],
     ),
   ];
 
@@ -135,7 +156,9 @@ class ChainsDataList {
       logo:
           'https://pbs.twimg.com/profile_images/1472933274209107976/6u-LQfjG_400x400.jpg',
       color: Color.fromARGB(255, 247, 0, 255),
-      rpc: ['https://api.mainnet-beta.solana.com'],
+      rpc: [
+        'https://rpc.walletconnect.org/v1',
+      ],
     ),
     const ChainMetadata(
       type: ChainType.solana,
@@ -143,7 +166,9 @@ class ChainsDataList {
       name: 'Solana Devnet',
       logo: 'https://avatars.githubusercontent.com/u/179229932?s=200&v=4',
       color: Color.fromARGB(255, 247, 0, 255),
-      rpc: ['https://api.devnet.solana.com'],
+      rpc: [
+        'https://rpc.walletconnect.org/v1',
+      ],
     ),
     const ChainMetadata(
       type: ChainType.solana,
@@ -152,7 +177,9 @@ class ChainsDataList {
       logo: 'https://avatars.githubusercontent.com/u/179229932?s=200&v=4',
       color: Colors.black,
       isTestnet: true,
-      rpc: ['https://api.testnet.solana.com'],
+      rpc: [
+        'https://rpc.walletconnect.org/v1',
+      ],
     ),
   ];
 
@@ -164,7 +191,7 @@ class ChainsDataList {
       logo: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png?v=040',
       color: Color.fromARGB(255, 255, 157, 0),
       rpc: [
-        'https://rpc.walletconnect.org/v1?chainId=bip122:000000000019d6689c085ae165831e93&projectId=${DartDefines.projectId}'
+        'https://rpc.walletconnect.org/v1',
       ],
     ),
     const ChainMetadata(
@@ -174,7 +201,7 @@ class ChainsDataList {
       logo: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png?v=040',
       color: Color.fromARGB(255, 255, 157, 0),
       rpc: [
-        'https://rpc.walletconnect.org/v1?chainId=bip122:000000000933ea01ad0ee984209779ba&projectId=${DartDefines.projectId}'
+        'https://rpc.walletconnect.org/v1',
       ],
       isTestnet: true,
     ),
@@ -253,7 +280,7 @@ class ChainsDataList {
       logo: 'https://cdn-icons-png.flaticon.com/512/12114/12114250.png',
       color: Color.fromARGB(255, 223, 0, 0),
       rpc: [
-        'https://api.trongrid.io',
+        'https://rpc.walletconnect.org/v1',
       ],
     ),
     const ChainMetadata(
@@ -264,6 +291,32 @@ class ChainsDataList {
       color: Color.fromARGB(255, 223, 0, 0),
       rpc: [
         'https://nile.trongrid.io',
+      ],
+      isTestnet: true,
+    ),
+  ];
+
+  static final List<ChainMetadata> tonChains = [
+    const ChainMetadata(
+      type: ChainType.tron,
+      chainId: 'ton:-239',
+      name: 'Ton Mainnet',
+      logo:
+          'https://pbs.twimg.com/profile_images/1931243733439115264/HfLnjCPR_400x400.jpg',
+      color: Color.fromARGB(255, 48, 207, 255),
+      rpc: [
+        'https://rpc.walletconnect.org/v1',
+      ],
+    ),
+    const ChainMetadata(
+      type: ChainType.tron,
+      chainId: 'ton:-3',
+      name: 'Ton Testnet',
+      logo:
+          'https://pbs.twimg.com/profile_images/1931243733439115264/HfLnjCPR_400x400.jpg',
+      color: Color.fromARGB(255, 48, 207, 255),
+      rpc: [
+        // 'https://rpc.walletconnect.org/v1', // TODO
       ],
       isTestnet: true,
     ),
