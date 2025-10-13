f1b1c41ecb5fcff9638128ac31e313f6afda9065
Alfreedom <00tango.bromine@icloud.com>
2025-10-13 13:37:02 +0200

    generated code

--- packages/reown_core/test/shared/shared_test_utils.mocks.dart
+++ packages/reown_core/test/shared/shared_test_utils.mocks.dart
@@ -1285,14 +1285,6 @@ class MockWebSocketHandler extends _i1.Mock implements _i32.WebSocketHandler {
   }
 
   @override
-  _i26.Future<void> get ready =>
-      (super.noSuchMethod(
-            Invocation.getter(#ready),
-            returnValue: _i26.Future<void>.value(),
-          )
-          as _i26.Future<void>);
-
-  @override
   _i26.Future<void> setup({required String? url}) =>
       (super.noSuchMethod(
             Invocation.method(#setup, [], {#url: url}),
