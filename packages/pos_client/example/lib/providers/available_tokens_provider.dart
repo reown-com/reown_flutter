b5d7a194582a8984793797b2e93f035ac66ac9e4
Alfreedom <00tango.bromine@icloud.com>
2025-10-21 11:06:04 +0200

    regerenerate packages (#277)

--- packages/pos_client/example/lib/providers/available_tokens_provider.dart
+++ packages/pos_client/example/lib/providers/available_tokens_provider.dart
@@ -5,157 +5,157 @@ import 'package:pos_client/models/pos_models.dart';
 
 final availableTokensProvider =
     StateNotifierProvider<TokenListNotifier, List<AvailableToken>>(
-  (ref) => TokenListNotifier(),
-);
+      (ref) => TokenListNotifier(),
+    );
 
 class TokenListNotifier extends StateNotifier<List<AvailableToken>> {
   TokenListNotifier()
-      : super([
-          // USDC on ETHEREUM
-          AvailableToken(
-            index: 1,
-            color: Colors.blue,
-            posToken: PosToken(
-              network: SupportedNetwork.ethereum.posNetwork,
-              symbol: 'USDC',
-              standard: 'erc20',
-              address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-            ),
-            selected: false,
+    : super([
+        // USDC on ETHEREUM
+        AvailableToken(
+          index: 1,
+          color: Colors.blue,
+          posToken: PosToken(
+            network: SupportedNetwork.ethereum.posNetwork,
+            symbol: 'USDC',
+            standard: 'erc20',
+            address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
           ),
-          // USDT on ETHEREUM
-          AvailableToken(
-            index: 2,
-            color: Colors.green,
-            posToken: PosToken(
-              network: SupportedNetwork.ethereum.posNetwork,
-              symbol: 'USDT',
-              standard: 'erc20',
-              address: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
-            ),
-            selected: false,
+          selected: false,
+        ),
+        // USDT on ETHEREUM
+        AvailableToken(
+          index: 2,
+          color: Colors.green,
+          posToken: PosToken(
+            network: SupportedNetwork.ethereum.posNetwork,
+            symbol: 'USDT',
+            standard: 'erc20',
+            address: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
           ),
-          // USDC on ARBITRUM
-          AvailableToken(
-            index: 3,
-            color: Colors.blue,
-            posToken: PosToken(
-              network: SupportedNetwork.arbitrum.posNetwork,
-              symbol: 'USDC',
-              standard: 'erc20',
-              address: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831',
-            ),
-            selected: false,
+          selected: false,
+        ),
+        // USDC on ARBITRUM
+        AvailableToken(
+          index: 3,
+          color: Colors.blue,
+          posToken: PosToken(
+            network: SupportedNetwork.arbitrum.posNetwork,
+            symbol: 'USDC',
+            standard: 'erc20',
+            address: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831',
           ),
-          // USDT on Arbitrum
-          AvailableToken(
-            index: 3,
-            color: Colors.blue,
-            posToken: PosToken(
-              network: SupportedNetwork.arbitrum.posNetwork,
-              symbol: 'USDT',
-              standard: 'erc20',
-              address: '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9',
-            ),
-            selected: false,
+          selected: false,
+        ),
+        // USDT on Arbitrum
+        AvailableToken(
+          index: 3,
+          color: Colors.blue,
+          posToken: PosToken(
+            network: SupportedNetwork.arbitrum.posNetwork,
+            symbol: 'USDT',
+            standard: 'erc20',
+            address: '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9',
           ),
-          // USDC on BASE
-          AvailableToken(
-            index: 4,
-            color: Colors.blue,
-            posToken: PosToken(
-              network: SupportedNetwork.base.posNetwork,
-              symbol: 'USDC',
-              standard: 'erc20',
-              address: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913',
-            ),
-            selected: false,
+          selected: false,
+        ),
+        // USDC on BASE
+        AvailableToken(
+          index: 4,
+          color: Colors.blue,
+          posToken: PosToken(
+            network: SupportedNetwork.base.posNetwork,
+            symbol: 'USDC',
+            standard: 'erc20',
+            address: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913',
           ),
-          // USDC on OP
-          AvailableToken(
-            index: 4,
-            color: Colors.blue,
-            posToken: PosToken(
-              network: SupportedNetwork.optimism.posNetwork,
-              symbol: 'USDC',
-              standard: 'erc20',
-              address: '0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85',
-            ),
-            selected: false,
+          selected: false,
+        ),
+        // USDC on OP
+        AvailableToken(
+          index: 4,
+          color: Colors.blue,
+          posToken: PosToken(
+            network: SupportedNetwork.optimism.posNetwork,
+            symbol: 'USDC',
+            standard: 'erc20',
+            address: '0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85',
           ),
-          // USDT on OP
-          AvailableToken(
-            index: 4,
-            color: Colors.blue,
-            posToken: PosToken(
-              network: SupportedNetwork.optimism.posNetwork,
-              symbol: 'USDT',
-              standard: 'erc20',
-              address: '0x94b008aA00579c1307B0EF2c499aD98a8ce58e58',
-            ),
-            selected: false,
+          selected: false,
+        ),
+        // USDT on OP
+        AvailableToken(
+          index: 4,
+          color: Colors.blue,
+          posToken: PosToken(
+            network: SupportedNetwork.optimism.posNetwork,
+            symbol: 'USDT',
+            standard: 'erc20',
+            address: '0x94b008aA00579c1307B0EF2c499aD98a8ce58e58',
           ),
-          // USDC on SEPOLIA
-          AvailableToken(
-            index: 5,
-            color: Colors.blue,
-            posToken: PosToken(
-              network: SupportedNetwork.sepolia.posNetwork,
-              symbol: 'USDC',
-              standard: 'erc20',
-              address: '0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238',
-            ),
-            selected: false,
+          selected: false,
+        ),
+        // USDC on SEPOLIA
+        AvailableToken(
+          index: 5,
+          color: Colors.blue,
+          posToken: PosToken(
+            network: SupportedNetwork.sepolia.posNetwork,
+            symbol: 'USDC',
+            standard: 'erc20',
+            address: '0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238',
           ),
-          // USDC on Solana
-          AvailableToken(
-            index: 6,
-            color: Colors.blue,
-            posToken: PosToken(
-              network: SupportedNetwork.solana.posNetwork,
-              symbol: 'USDC',
-              standard: 'token',
-              address: 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
-            ),
-            selected: false,
+          selected: false,
+        ),
+        // USDC on Solana
+        AvailableToken(
+          index: 6,
+          color: Colors.blue,
+          posToken: PosToken(
+            network: SupportedNetwork.solana.posNetwork,
+            symbol: 'USDC',
+            standard: 'token',
+            address: 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
           ),
-          // USDC on Solana Devnet
-          AvailableToken(
-            index: 6,
-            color: Colors.blue,
-            posToken: PosToken(
-              network: SupportedNetwork.solanaDevnet.posNetwork,
-              symbol: 'USDC',
-              standard: 'token',
-              address: 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
-            ),
-            selected: false,
+          selected: false,
+        ),
+        // USDC on Solana Devnet
+        AvailableToken(
+          index: 6,
+          color: Colors.blue,
+          posToken: PosToken(
+            network: SupportedNetwork.solanaDevnet.posNetwork,
+            symbol: 'USDC',
+            standard: 'token',
+            address: 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
           ),
-          // USDT on Tron
-          AvailableToken(
-            index: 7,
-            color: Colors.blue,
-            posToken: PosToken(
-              network: SupportedNetwork.tron.posNetwork,
-              symbol: 'USDT',
-              standard: 'trc20',
-              address: 'TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t',
-            ),
-            selected: false,
+          selected: false,
+        ),
+        // USDT on Tron
+        AvailableToken(
+          index: 7,
+          color: Colors.blue,
+          posToken: PosToken(
+            network: SupportedNetwork.tron.posNetwork,
+            symbol: 'USDT',
+            standard: 'trc20',
+            address: 'TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t',
           ),
-          // USDT on Tron Testnet
-          AvailableToken(
-            index: 7,
-            color: Colors.blue,
-            posToken: PosToken(
-              network: SupportedNetwork.tronTestnet.posNetwork,
-              symbol: 'USDT',
-              standard: 'trc20',
-              address: 'TXYZopYRdj2D9XRtbG411XZZ3kM5VkAeBf',
-            ),
-            selected: false,
+          selected: false,
+        ),
+        // USDT on Tron Testnet
+        AvailableToken(
+          index: 7,
+          color: Colors.blue,
+          posToken: PosToken(
+            network: SupportedNetwork.tronTestnet.posNetwork,
+            symbol: 'USDT',
+            standard: 'trc20',
+            address: 'TXYZopYRdj2D9XRtbG411XZZ3kM5VkAeBf',
           ),
-        ]);
+          selected: false,
+        ),
+      ]);
 
   void select(AvailableToken selected) {
     state = [
