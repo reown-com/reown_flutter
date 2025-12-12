c8def876bf97648695348f79973e19d83f8187e3
Alfreedom <00tango.bromine@icloud.com>
2025-12-12 17:24:29 +0100

    remove comments

--- packages/reown_yttrium/android/src/main/kotlin/com/reown/reown_yttrium/WalletConnectPay.kt
+++ packages/reown_yttrium/android/src/main/kotlin/com/reown/reown_yttrium/WalletConnectPay.kt
@@ -4,7 +4,6 @@ import io.flutter.plugin.common.MethodChannel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import uniffi.yttrium.WalletConnectPay
 import uniffi.yttrium.WalletConnectPayJson
 
 class WalletConnectPay {
@@ -12,24 +11,17 @@ class WalletConnectPay {
         private lateinit var walletConnectPay: WalletConnectPayJson
 
         fun initialize(params: Any?, result: MethodChannel.Result) {
-            val jsonString = params as? String ?: return result.error("WalletConnectPay", "Invalid parameters", null)
-//            val dict = jsonStringToMap(jsonString)
-//            val baseUrl = dict["baseUrl"] as? String ?: return result.error("WalletConnectPay", "Missing baseUrl", null)
-
-            walletConnectPay = WalletConnectPayJson(jsonString)
+            val baseUrl = params as? String ?: return result.error("WalletConnectPay", "Invalid parameters", null)
+            walletConnectPay = WalletConnectPayJson(baseUrl)
             result.success(true)
         }
 
         fun createPayment(params: Any?, result: MethodChannel.Result) {
-            val jsonString = params as? String ?: return result.error("WalletConnectPay", "Invalid parameters", null)
-//            val dict = jsonStringToMap(jsonString)
-//            val referenceId = dict["referenceId"] as? String ?: return result.error("WalletConnectPay", "Missing paymentId", null)
-//            val amountUnit = dict["amountUnit"] as? String ?: return result.error("WalletConnectPay", "Missing paymentId", null)
-//            val amountValue = dict["amountValue"] as? String ?: return result.error("WalletConnectPay", "Missing paymentId", null)
+            val requestJson = params as? String ?: return result.error("WalletConnectPay", "Invalid parameters", null)
 
             CoroutineScope(Dispatchers.IO).launch {
                 try {
-                    val createPaymentResponse = walletConnectPay.createPayment(jsonString)
+                    val createPaymentResponse = walletConnectPay.createPayment(requestJson)
                     result.success(createPaymentResponse)
                 } catch (e: Exception) {
                     result.error("WalletConnectPay", "getPayment: ${e.message}", null)
