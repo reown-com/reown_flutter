5196fe9f487c96db14c8055101ede64daf37e05a
Alfreedom <00tango.bromine@icloud.com>
2026-01-15 11:39:40 +0100

    rollback

--- packages/pos_client/generate_files.sh
+++ packages/pos_client/generate_files.sh
@@ -4,7 +4,7 @@ flutter clean
 flutter pub get
 dart run build_runner build --delete-conflicting-outputs
 # dart pub outdated --no-dev-dependencies --up-to-date --no-dependency-overrides
-# dart format .
+dart format .
 # dart run dependency_validator
 
 cd example
@@ -15,7 +15,7 @@ flutter clean
 flutter pub get
 dart run build_runner build --delete-conflicting-outputs
 # dart pub outdated --no-dev-dependencies --up-to-date --no-dependency-overrides
-# dart format .
+dart format .
 # dart run dependency_validator
 
 cd ios
--- packages/reown_cli/generate_files.sh
+++ packages/reown_cli/generate_files.sh
@@ -5,5 +5,5 @@ rm pubspec.lock
 flutter pub get
 # dart run build_runner build --delete-conflicting-outputs
 # dart pub outdated --no-dev-dependencies --up-to-date --no-dependency-overrides
-# dart format .
+dart format .
 # dart run dependency_validator
