3035aff2aa34ba23b3bea83f160909d4ff989928
Alfreedom <00tango.bromine@icloud.com>
2026-01-15 11:39:08 +0100

    rollback scripts

--- packages/reown_cli/generate_files.sh
+++ packages/reown_cli/generate_files.sh
@@ -3,7 +3,7 @@
 flutter clean
 rm pubspec.lock
 flutter pub get
-# dart run build_runner build #--delete-conflicting-outputs
+# dart run build_runner build --delete-conflicting-outputs
 # dart pub outdated --no-dev-dependencies --up-to-date --no-dependency-overrides
 # dart format .
 # dart run dependency_validator
